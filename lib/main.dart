f386ebb061dbd3cccbc354ba34f623c4c0445d4e
Mario Paz <mariopaz5399@gmail.com>
2024-08-10 18:03:43 -0600

    agregar

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainScreen(),
+    ); 
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String _messageFromChild = '';
+
+  void _receiveMessage(String message) {
+    setState(() {
+      _messageFromChild = message;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Tab Navigation Example'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.message)),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Option 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Option 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: ElevatedButton(
+                child: Text('Open Details Page'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => InfoScreen()),
+                  );
+                },
+              ),
+            ),
+            MessageWidget(
+              onSendMessage: _receiveMessage,
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(_messageFromChild)),
+            );
+          },
+          child: Icon(Icons.info),
+        ),
+      ),
+    );
+  }
+}
+
+class InfoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Info Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Return to Main'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class MessageWidget extends StatelessWidget {
+  final ValueChanged<String> onSendMessage;
+
+  MessageWidget({required this.onSendMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextField(
+          onSubmitted: (text) {
+            onSendMessage(text);
+          },
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Enter message',
+          ),
+        ),
+        ElevatedButton(
+          child: Text('Send to Parent'),
+          onPressed: () {
+            onSendMessage('Hello from child widget!');
+          },
+        ),
+      ],
+    );
+  }
+}
